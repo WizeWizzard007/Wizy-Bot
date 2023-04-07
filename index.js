5d164d28caffb0f7d3208eb264f8c213b24fe4c9
WizeWizzard <wizewizard007@gmail.com>
2023-04-07 03:52:52 -0400

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-
+const { SlashCommandBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
